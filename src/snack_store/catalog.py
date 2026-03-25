974b855341d37bd1506079edfa9b4fd2fff530c7
sunqinxue2026 <sunqinxue2026@163.com>
2026-03-25 19:01:15 +0800

    feat: add snack backend mvp

---
+++ src/snack_store/catalog.py
@@ -0,0 +1,9 @@
+SNACKS = [
+    {"id": 1, "name": "海盐薯片", "price": 12, "stock": 36},
+    {"id": 2, "name": "抹茶曲奇", "price": 18, "stock": 24},
+    {"id": 3, "name": "草莓软糖", "price": 9, "stock": 52},
+]
+
+
+def list_snacks():
+    return SNACKS
